fdb8ef39768c59813e01fd976e0f6bc9dd4c549a
Gayathri <gayuguru111@gmail.com>
2025-07-25 13:13:48 +0530

    new files

---
+++ javaScript/DOM/dom1.js
@@ -0,0 +1,12 @@
+let box1=document.getElementsByClassName('box')
+console.log(box1);
+let box2=document.getElementById('li1')
+console.log(li1);
+
+let box3=document.getElementsByTagName('li')
+console.log(box3);
+console.log(box3[1]);
+let box4=document.querySelector('li')
+console.log(box4);
+let box5=document.querySelectorAll('li')
+console.log(box5);
