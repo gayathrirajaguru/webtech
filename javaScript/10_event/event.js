6f34519910cdd2fe99b0d2950179c4199eb706a3
Gayathri <gayuguru111@gmail.com>
2025-07-26 13:30:42 +0530

    new files

---
+++ javaScript/10_event/event.js
@@ -0,0 +1,17 @@
+console.log("hii");
+let login=()=>{
+    console.log("login done ");
+    
+}
+let changeColor=()=>{
+    let main=document.querySelector("main")
+    main.style.backgroundColor="orange"
+    console.log("changed");
+    
+}
+let changeColor2=()=>{
+    let main=document.querySelector("main")
+    main.style.backgroundColor="white"
+    console.log("changed");
+    
+}
---
+++ javaScript/10_event/task.js
@@ -0,0 +1,33 @@
+let green=()=>{
+    let box1=document.querySelector(".box");
+    box1.style.backgroundColor="green";
+    
+}
+let blue=()=>{
+    let box1=document.querySelector(".box");
+    box1.style.backgroundColor="blue";
+    
+}
+let red=()=>{
+    let box1=document.querySelector(".box");
+    box1.style.backgroundColor="red";
+    
+}
+let pink=()=>{
+    let box1=document.querySelector(".box");
+    box1.style.backgroundColor="pink";
+    
+}
+
+let change1=()=>{
+    let box1=document.querySelector(".box");
+    box1.classList.toggle('dark')
+    
+}
+
+//! let box1=document.querySelector(".box");
+//!let changeColor=(color)=>{
+    //! outer.style.backgroundColor=color
+    //!  }
+//html
+//<button class="btn1" onclick="changeColor('red')"> red</button>
