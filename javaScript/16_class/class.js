02c95fc9670995eab64623f5a99e7938e9d5f8a2
Gayathri <gayuguru111@gmail.com>
2025-08-12 19:40:17 +0530

    new files

---
+++ javaScript/16_class/class.js
@@ -0,0 +1,18 @@
+class std{
+    sname;
+    age;
+    constructor(sname,age){
+        this.sname=sname
+        this.age=age
+    }
+   
+}
+ let s1=new std("virat",10)
+ let s2=new std("rohit",45)
+ console.log(s1.sname);
+ console.log(s1.age);
+ console.log(s2.age,s2.sname);
+
+ 
+ 
+ 
